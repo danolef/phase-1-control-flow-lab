2e1e0c4acb124c18f388747a00c13410deb8767f
Dan <dt.lefever@gmail.com>
2021-10-11 15:37:25 -0600

    completed assignement

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distanace){
   // Write your code here!
+  if (distanace <= 400) {
+    return 'This one is on me!'
+  } else if (distanace > 2500) {
+    return 'No can do.'
+  } else if (distanace > 2000) {
+    return 'I will gladly take your thirty bucks.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+ return city ==="NYC"? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
   // Write your code here!
 }
